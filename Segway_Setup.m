044f83bc70b8ae58f1711db7df1e348139116abc
GarrisonJohnston123 <garrisoj@andrew.cmu.edu>
2018-01-06 17:10:24 -0500

    Added State space matrices to setup

--- Segway_Setup.m
+++ Segway_Setup.m
@@ -1,8 +1,15 @@
 %% This script sets up the variables for the segway project
 %% Author: Garrison Johnston
 %% Constants
+x0 = [0,0,15*pi/180,0]'; % Initial conditions
+tstop = 10;
+%% Linearization
+V = 0;
+lin_point = [0, 0, 0, 0]'; % Linearization point
+[A, B] = GetLinModFtxu(@segway_ode, [], lin_point, V);
 Rw = 0.0216;            % radious of wheel [m]
 Ts = 0.005;             % Step Size
+%% LQR
 Q = [500, 0,  0,   0;
      0,   50, 0,   0;
      0,   0,  500, 0;
